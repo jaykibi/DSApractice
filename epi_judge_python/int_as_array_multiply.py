c3b1dbf0bf6a17972db3d8d14001f7c443454a05
Raquibbi Muhammaddi <muhammaddiraquibbi@contractor.bfusa.com>
2022-04-07 23:44:54 -0500

    did 5.3

--- epi_judge_python/int_as_array_multiply.py
+++ epi_judge_python/int_as_array_multiply.py
@@ -4,11 +4,35 @@ from test_framework import generic_test
 
 
 def multiply(num1: List[int], num2: List[int]) -> List[int]:
-    # TODO - you fill in here.
-    return []
+    sign = 1
+    if num1[0] == 0 or num2[0] == 0:
+        return [0]
+    if num1[0] < 0 or num2[0] < 0:  # checking if a number is negative
+        if num1[0] < 0 and num2[0] < 0:  # if first num of both are negative, set sign to 1, answer will be positive
+            sign = 1
+        elif num1[0] < 0 or num2[0] < 0:  # if first number of either list is negative switch sign variable to -1
+            sign = -1  # need to multiply entire number by sign at the end
+        num1[0] = abs(num1[0])
+        num2[0] = abs(num2[0])
+
+    result = [0] * (len(num1) + len(num2))
+    for num1_idx in reversed(range(len(num1))):
+        for num2_idx in reversed(range(len(num2))):
+            result[num1_idx + num2_idx + 1] += num1[num1_idx] * num2[num2_idx] #
+            result[num1_idx + num2_idx] += result[num1_idx + num2_idx + 1] // 10
+            result[num1_idx + num2_idx + 1] %= 10
+
+    number_start = 0
+    if result[0] == 0:
+        for idx in range(len(result)):
+            if result[idx] == 0:
+                number_start += 1
+            else:
+                break
+        result = result[number_start:]
+    return [sign * result[0]] + result[1:]
 
 
-if __name__ == '__main__':
-    exit(
-        generic_test.generic_test_main('int_as_array_multiply.py',
-                                       'int_as_array_multiply.tsv', multiply))
+n1 = [1, 3, 1, 4, 1, 2]
+n2 = [-1, 3, 1, 3, 3, 3, 2]
+print(multiply(n1, n2))
